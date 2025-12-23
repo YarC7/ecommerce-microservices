1aa9c8a99f4fa4bd382afbfe1b04243c7c270c29
Cankkun <nguyenduccanh177@gmail.com>
2025-12-23 15:44:50 +0700

    feat: add unit and integration tests for order service, including setup and mock implementations

--- microservices/order-service/tests/order_controller_test.go
+++
@@ -1,4 +0,0 @@
-// Package tests contains both unit and integration tests for the order service
-// Unit tests are in the /unit subdirectory
-// Integration tests are in the /integration subdirectory
-package tests
